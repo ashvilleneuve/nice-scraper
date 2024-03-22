a5374fa5588919f5ca1776ec34674ee4b07a9506
ashvilleneuve <ashley.villeneuve@shopify.com>
2024-03-22 09:13:30 -0700

    bug fixes

--- updater.sh
+++ updater.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
-# Initial setup
 last_file=""
 current_file=""
 
 while true
-do
-    # Find the two most recent files
-    newest_file=$(ls -t ~/Downloads/events*.ics | head -n 1)
-    second_newest_file=$(ls -t ~/Downloads/events*.ics | head -n 2 | tail -n 1)
+  do
+      newest_file=$(ls -t ~/Downloads/events*.ics | head -n 1)
+      second_newest_file=$(ls -t ~/Downloads/events*.ics | head -n 2 | tail -n 1)
+      file_count=$(ls ~/Downloads/events*.ics | wc -l | tr -d ' ')
 
-    # If there are two files and they are different from the last two files
-    if [ -n "$newest_file" ] && [ -n "$second_newest_file" ] && [ "$newest_file" != "$current_file" ] && [ "$second_newest_file" != "$last_file" ]
-    then
-        # Open the newest file
-        open "$newest_file"
+      if [[ $file_count -gt 2 ]] 
+      then
+      find ~/Downloads -name "events*.ics" -print0 | xargs -0 ls -t | tail -n +3 | tr '\n' '\0' | xargs -0 rm --
+      fi
 
-        # If there was a last file, delete it
-        if [ -n "$last_file" ]
-        then
-            rm "$last_file"
-        fi
 
-        # Update the last and current files
-        last_file=$current_file
-        current_file=$newest_file
-    fi
+      if [ -n "$newest_file" ] && [ -n "$second_newest_file" ] && [ "$newest_file" != "$current_file" ] && [ "$second_newest_file" != "$last_file" ]
+      
+      then
+          diff "$newest_file" "$second_newest_file"
+          cmp -l "$newest_file" "$second_newest_file"
+          if ! cmp -s "$newest_file" "$second_newest_file"
+          
+          then
+              echo "Schedule has changed, please import to calendar"
+              open "$newest_file"
 
-    sleep 60
-done
+              if [ -n "$last_file" ]
+              then
+                  rm "$last_file"
+              fi
+
+              last_file=$current_file
+              current_file=$newest_file
+          fi
+      fi
+      sleep 60
+  done
